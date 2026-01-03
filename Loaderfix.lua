4506786e62302707575eb71ca8f84330df66c400
Veristok <ldoenewplayer3@gmail.com>
2026-01-04 01:05:11 +0300

    Update Loaderfix.lua

--- Loaderfix.lua
+++ Loaderfix.lua
@@ -1,5 +1,8 @@
 --// Obsidian UI
-local Obsidian = loadstring(game:HttpGet("https://raw.githubusercontent.com/ObsidianLib/Obsidian/main/source.lua"))()
+local repo = "https://raw.githubusercontent.com/deividcomsono/Obsidian/main/"
+local Library = loadstring(game:HttpGet(repo .. "Library.lua"))()
+local ThemeManager = loadstring(game:HttpGet(repo .. "addons/ThemeManager.lua"))()
+local SaveManager = loadstring(game:HttpGet(repo .. "addons/SaveManager.lua"))()
 
 --// Window
 local Window = Obsidian:CreateWindow({
