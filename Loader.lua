f3da6ce5c3255bd281aa930b8b3891aca416bc6f
Veristok <ldoenewplayer3@gmail.com>
2026-01-04 00:37:38 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,36 +1,48 @@
---Лоадер для игр--
+-- Лоадер для игр
 
---// Services
+-- Services
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
---// Obsidian UI
-local Obsidian = loadstring(game:HttpGet("https://raw.githubusercontent.com/ObsidianLib/Obsidian/main/Library.lua"))()
+-- Обновлённые ссылки на Obsidian UI
+local ObsidianLibURL = "https://raw.githubusercontent.com/deividcomsono/Obsidian/main/Library.lua"
+local ObsidianThemeURL = "https://raw.githubusercontent.com/deividcomsono/Obsidian/main/addons/ThemeManager.lua"
+local ObsidianSaveURL  = "https://raw.githubusercontent.com/deividcomsono/Obsidian/main/addons/SaveManager.lua"
 
---// Window
+-- Загружаем UI библиотеку
+local success, Obsidian = pcall(function()
+    return loadstring(game:HttpGet(ObsidianLibURL, true))()
+end)
+
+if not success or not Obsidian then
+    warn("Не удалось загрузить Obsidian UI библиотеку")
+    return
+end
+
+-- Window
 local Window = Obsidian:CreateWindow({
     Title = "mqixwed",
     Center = true,
     AutoShow = true
 })
 
---// Tab
+-- Tab
 local MainTab = Window:AddTab("Loader")
 
---// Info
+-- Info
 MainTab:AddParagraph({
     Title = "Loader",
     Content = "Загрузка подходящего скрипта для игры...\nПожалуйста, подождите."
 })
 
---// Supported games
+-- Supported games
 local Games = {
     [13985151437] = function() -- CASUAL STOCK
         loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/69cc0b3eb4a7f175d4d4e60d29007bf92e7d8e2f7250f90d75f445d383f2537f/download"))()
     end
 }
 
---// Loader function
+-- Loader function
 task.delay(1.5, function()
     local PlaceId = game.PlaceId
 
@@ -49,7 +61,7 @@ task.delay(1.5, function()
     else
         MainTab:AddParagraph({
             Title = "Игра не поддерживается",
-            Content = "Для этой игры пока нет скрипта."
+            Content = "Для этой игре пока нет скрипта."
         })
     end
 end)
